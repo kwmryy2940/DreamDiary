68725b59843b7ab95c4b604e43ebf4b749c5d4ce
kwmryy2940 <mailbox.kawamura@gmail.com>
2024-05-13 11:10:02 +0900

    カレンダーのレイアウト変更

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -25,15 +25,23 @@
 
   <v-dialog v-model="contentListDialog">
     <v-sheet>
-    <v-card
-    v-for="content in filterdContents.sort((a,b)=>{return b.id-a.id})"
-            :key="content"
-            class="ma-2"
-            variant="outlined">
-            <v-card-text  class="text-pre-wrap">{{ content.title }}</v-card-text>
-            <v-card-subtitle><v-icon>mdi-update</v-icon>{{ content.timestamp }}</v-card-subtitle>
-    </v-card>
-  </v-sheet>
+      <v-card
+        v-for="content in filterdContents.sort((a, b) => {
+          return b.id - a.id;
+        })"
+        :key="content"
+        class="ma-2"
+        variant="outlined"
+      >
+        <v-card-text class="text-pre-wrap">{{ content.title }}</v-card-text>
+        <v-card-subtitle>{{
+          categoryItem[content.categoryId].text
+        }}</v-card-subtitle>
+        <v-card-subtitle
+          ><v-icon>mdi-update</v-icon>{{ content.timestamp }}</v-card-subtitle
+        >
+      </v-card>
+    </v-sheet>
   </v-dialog>
 </template>
 
@@ -54,41 +62,71 @@ const contentDate = ref(null);
 
 const filterdContents = ref([]);
 
-const handleDateClick = (info) => {
+const categoryItem = [
+  { value: 0, text: "吉夢", color: "#00E676" },
+  { value: 1, text: "悪夢", color: "#FF1744" },
+  { value: 2, text: "その他", color: "#00B0FF" },
+];
 
+const handleDateClick = (e) => {
   filterdContents.value = calendarOptions.value.events.filter(
-    (item) => item.start === info.dateStr
+    (item) => item.start === e.dateStr
   );
-  if(filterdContents.value.length<=0)return;
+  if (filterdContents.value.length <= 0) return;
 
-  contentListDialog.value=true;
+  contentListDialog.value = true;
 };
 
-const handleEventClick = (info) => {
-  contentText.value = info.event._def.title;
-  contentDate.value = info.event._instance.range.start;
+const handleEventClick = (e) => {
+  contentText.value = e.event._def.title;
+  contentDate.value = e.event._instance.range.start;
   contentDialog.value = true;
+};
+
+const handleDayRender = (e) => {
+  console.log(e);
 };
 
 const calendarOptions = ref({
   locale: "ja",
   plugins: [dayGridPlugin, interactionPlugin],
+  initialView: "dayGridMonth",
   headerToolbar: {
-    start: "prev", // 前月表示させるボタン
-    center: "title", // 表示している年月のラベル
-    end: "next", // 次月表示ボタン
+    start: "title",
+    center: "",
+    end: "today dayGridMonth,dayGridWeek,dayGridDay prev,next",
   },
-  events: [
-    {
-      title: "5/11の予定",
-      start: "2024-05-11",
-    },
-  ],
+  buttonText: {
+    prev:"＜前月",
+    next:"次月＞",
+    today: "今日",
+    month: "月",
+    week: "週",
+    day: "日",
+  },
+  events: [{}],
   // 日付クリック時の処理
   dateClick: handleDateClick,
 
   // eventクリック時の処理
   eventClick: handleEventClick,
+
+  // 日付表記を変更する処理
+  dayCellContent: function (e) {
+    e.dayNumberText = e.dayNumberText.replace(/日$/, "");
+    return e.dayNumberText;
+  },
+  // dayHeaderContent  :function(date,cell){
+  //   console.log(date);
+  //   console.log(cell);
+
+  //   return date.text
+  // }
+  //  // 曜日の表記の指定
+  //  dayHeaderContent: function(e) {
+  //       console.log(e);
+  //       return e.text;
+  //   },
 });
 
 async function loadItems() {
@@ -104,11 +142,12 @@ async function loadItems() {
 
   contents.value.forEach((content) => {
     const event = {
-      id:content.id,
-      categoryId:content.categoryId,
+      id: content.id,
+      categoryId: content.categoryId,
       title: content.dreamContent,
       start: content.timestamp.substring(0, 10),
-      timestamp:content.timestamp
+      color: categoryItem[content.categoryId].color,
+      timestamp: content.timestamp,
     };
 
     calendarOptions.value.events.push(event);
@@ -124,3 +163,13 @@ onMounted(async () => {
   await loadItems();
 });
 </script>
+
+<style>
+.fc-day-sat{
+  color: blue;
+}
+
+.fc-day-sun{
+    color: red;
+}
+</style>
